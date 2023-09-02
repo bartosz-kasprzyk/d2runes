236ea51a8332edc8174d6ab8effea7d114e60c00
Bartosz Kasprzyk <bartosz.kasprzyk58@gmail.com>
2023-09-02 21:01:39 +0200

    added functions in JS

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
-const buttonElement = document.querySelector(".js-button");
-const body = document.querySelector(".js-body");
-const textChange = document.querySelector(".js-textChange");
+{
+    const toggleGradient = (event) => {
+        const body = document.querySelector(".js-body");
+        const textChange = document.querySelector(".js-textChange");
 
-buttonElement.addEventListener("click", () => {
-    body.classList.toggle("gradient");
-    textChange.innerText = (body.classList.contains("gradient")) ? "Wyłącz" : "Włącz"
-});
+        body.classList.toggle("gradient");
+        textChange.innerText = (body.classList.contains("gradient")) ? "Wyłącz" : "Włącz";
+    };
+
+    const init = () => {
+        const buttonElement = document.querySelector(".js-button");
+
+        buttonElement.addEventListener("click", toggleGradient);
+    };
+
+    init();
+}
